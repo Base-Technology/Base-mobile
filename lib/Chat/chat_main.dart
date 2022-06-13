f1d66b4886a49e6d53a9ba06403991debd6ebfc2
Yizhen Liu <62583086+edwardslau@users.noreply.github.com>
2022-06-12 23:42:40 -0600

    Chat Page Main Menu

---
+++ lib/Chat/chat_main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+import '../Homepage/bottom_nav_bar.dart';
+import 'chat_page.dart';
+
+class ChatMain extends StatelessWidget {
+  const ChatMain({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: ChatPage(),
+    );
+  }
+}
---
+++ lib/Chat/models/chat_user_model.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/cupertino.dart';
+
+class ChatUser{
+  String name;
+  String message;
+  String profile_pic_URL;
+  String time;
+  ChatUser({required this.name,required this.message, required this.profile_pic_URL, required this.time});
+}
